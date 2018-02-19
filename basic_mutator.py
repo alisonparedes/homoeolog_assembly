6c2f435f19413a02ba99f593c6f0365f17ce39cf
alison <ap2053@wildcats.unh.edu>
2018-02-19 11:02:53 -0500

    Initial commit

---
+++ basic_mutator.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file_name")
+    parser.add_argument("k")
+    args = parser.parse_args()
