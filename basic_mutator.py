ebdeacf91ff5743434f87a77fff175ea16b709fb
Alison Paredes <ap2053@wildcats.unh.edu>
2018-02-19 13:24:45 -0500

    Calculates the SNP density distribution

--- basic_mutator.py
+++ basic_mutator.py
@@ -6,7 +6,8 @@ Example command line: python basic_mutator.py test_01.fna 1 ALISON
 """
 
 import random
-
+import heapq
+import copy
 
 def read_file(file_name):
     sequence = ''
@@ -30,7 +31,6 @@ mutation_model = {
 
 def sample_positions(sequence, k, seed):
     random.seed(seed)
-    new_sequence = list(sequence)
     last_position = len(sequence) - 1
     k_random_positions = random.sample(range(last_position), k)
     return k_random_positions
@@ -45,10 +45,26 @@ def mutate(sequence, k_random_positions):
     return ''.join(new_sequence)
 
 
-def snp_density(sequence, snp_positions):
+def snp_density(sequence, k):
     total_bp = len(sequence)
-    snp_count = len(snp_positions)
-    return snp_count / total_bp
+    return k / total_bp
+
+
+def snp_density_distribution(sequence, snp_positions, interval):
+    min_heap = copy.copy(snp_positions)
+    heapq.heapify(min_heap)
+    end_position = interval - 1
+    count = 0
+    distribution = {}
+    while end_position < len(sequence) + interval:
+        if len(min_heap) > 0 and min_heap[0] <= end_position:
+            heapq.heappop(min_heap)
+            count += 1
+        else:
+            end_position += interval
+            distribution[count] = distribution.get(count, 0) + 1
+            count = 0
+    return distribution
 
 
 if __name__ == "__main__":
@@ -59,11 +75,15 @@ if __name__ == "__main__":
     parser.add_argument("k")
     parser.add_argument("seed")
     args = parser.parse_args()
+    k = int(args.k)
 
     orig_sequence, header = read_file(args.file_name)
-    random_positions = sample_positions(orig_sequence, int(args.k), args.seed)
+    random_positions = sample_positions(orig_sequence, k, args.seed)
     new_sequence = mutate(orig_sequence, random_positions)
-
-    print("{0} | snp density: {1}".format(header, snp_density(orig_sequence, random_positions)))
+    snp_density = snp_density(orig_sequence, k)
+    snp_distribution = snp_density_distribution(new_sequence, random_positions, interval=100)
+    print("{0} | snp density: {1} snp density distribution: {2}".format(header,
+                                                                        snp_density,
+                                                                        snp_distribution))
     print("{0}".format(new_sequence))
 
