62197c5c96b59a9b9d5e95d6d4b0a38d3b849ea7
Alison Paredes <ap2053@wildcats.unh.edu>
2018-04-18 17:38:02 -0400

    Prints csplit command

--- report_quality.py
+++ report_quality.py
@@ -11,7 +11,9 @@ if __name__ == "__main__":
     for line in sys.stdin:
         contig_file = line.strip()
         os.system("cat {0} lambdaphage.fna | muscle -out ref_contig01.fa".format(contig_file))
-        os.system("csplit --prefix ref_{0}_ ref_{0}.fa \"/>/\" \"{*}\"".format(contig_file))
+        command = "csplit --prefix ref_{0}_ ref_{0}.fa \"/>/\" \"{*}\"".format(contig_file)
+        print(command)
+        os.system(command)
         os.system("python compare_fasta.py lambdaphage.fna ref_{0}_01 ref_{0}.vcf".format(contig_file))
         report_id = random.randint(0,1000)
         os.system("touch {0}_assembly_quality_{1}.tsv".format(args.ref_file, report_id))
