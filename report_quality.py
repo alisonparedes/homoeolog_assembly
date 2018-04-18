7cfdad4e290ab852fb46becd73c85910f3368821
Alison Paredes <ap2053@wildcats.unh.edu>
2018-04-18 17:27:10 -0400

    Omits path

--- report_quality.py
+++ report_quality.py
@@ -12,8 +12,8 @@ if __name__ == "__main__":
         contig_file = line.strip()
         os.system("cat {0} lambdaphage.fna | muscle -out ref_contig01.fa".format(contig_file))
         os.system("csplit --prefix ref_{0}_ ref_{0}.fa \"/>/\" \"{*}\"".format(contig_file))
-        os.system("python homoeolog_assembly/compare_fasta.py lambdaphage.fna ref_{0}_01 ref_{0}.vcf".format(contig_file))
+        os.system("python compare_fasta.py lambdaphage.fna ref_{0}_01 ref_{0}.vcf".format(contig_file))
         report_id = random.randint(0,1000)
         os.system("touch {0}_assembly_quality_{1}.tsv".format(args.ref_file, report_id))
         for i in range(1, 4):
-            os.system("python homoeolog_assembly/compare_vcf.py {0}_0{1}.vcf ref_{1}.vcf >> {0}assembly_quality_{2}.tsv".format(args.ref_file, i, contig_file))
+            os.system("python compare_vcf.py {0}_0{1}.vcf ref_{1}.vcf >> {0}assembly_quality_{2}.tsv".format(args.ref_file, i, contig_file))
