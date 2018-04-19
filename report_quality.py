33ab7c9aed0c5cc73fd15c53f1b8784d60084c3a
Alison Paredes <ap2053@wildcats.unh.edu>
2018-04-18 20:14:46 -0400

    Prints csplit command

--- report_quality.py
+++ report_quality.py
@@ -8,6 +8,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("ref_file")
     args = parser.parse_args()
+    report_id = random.randint(0,1000)
+    os.system("touch {0}_assembly_quality_{1}.tsv".format(args.ref_file, report_id))
 
     for line in sys.stdin:
         contig_file = line.strip()
@@ -16,7 +18,5 @@ if __name__ == "__main__":
         command += "'/>/' '{*}'"
         os.system(command)
         os.system("python compare_fasta.py lambdaphage.fna ref_{0}_01 ref_{0}.vcf".format(contig_file))
-        report_id = random.randint(0,1000)
-        os.system("touch {0}_assembly_quality_{1}.tsv".format(args.ref_file, report_id))
         for i in range(1, 4):
-            os.system("python compare_vcf.py {0}_0{1}.vcf ref_{1}.vcf >> {0}assembly_quality_{2}.tsv".format(args.ref_file, i, contig_file))
+            os.system("python compare_vcf.py {0}_0{1}.vcf ref_{2}.vcf >> {0}assembly_quality_{3}.tsv".format(args.ref_file, i, contig_file, report_id))
