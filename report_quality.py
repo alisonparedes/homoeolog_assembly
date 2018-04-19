4fa220f1614f1e177bd0d68c7ab7aea514d1743c
Alison Paredes <ap2053@wildcats.unh.edu>
2018-04-18 20:26:40 -0400

    Parameterizes csplit

--- report_quality.py
+++ report_quality.py
@@ -14,9 +14,9 @@ if __name__ == "__main__":
     for line in sys.stdin:
         contig_file = line.strip()
         os.system("cat {0} lambdaphage.fna | muscle -out ref_{0}.fa".format(contig_file))
-        command = "csplit --prefix ref_{0}_ ref_{0}.fa ".format("a file")
+        command = "csplit --prefix ref_{0}_ ref_{0}.fa ".format(contig_file)
         command += "'/>/' '{*}'"
         os.system(command)
-        os.system("python homoeolog_assembly/compare_fasta.py lambdaphage.fna ref_{0}_01 ref_{0}.vcf".format(contig_file))
+        os.system("python homoeolog_assembly/compare_fasta.py ref_{0}_01 lambdaphage.fna ref_{0}.vcf".format(contig_file))
         for i in range(1, 4):
             os.system("python homoeolog_assembly/compare_vcf.py {0}_0{1}.vcf ref_{2}.vcf >> {0}assembly_quality_{3}.tsv".format(args.ref_file, i, contig_file, report_id))
