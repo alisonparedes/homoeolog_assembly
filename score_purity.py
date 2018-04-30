d04ce5c9be12a990d65baa66b7d89ab522341e7d
Alison Paredes <ap2053@wildcats.unh.edu>
2018-04-30 18:05:59 -0400

    Summarize delta explained by homoeolog or homolog

--- score_purity.py
+++ score_purity.py
@@ -6,10 +6,10 @@ def read_tsv(file_name):
     purity = {}
     while True:
         try:
-            haplotype, contig, contig_purity = next(tsv_file)
+            haplotype, contig, contig_purity, delta_homoeolog, delta_homolog = next(tsv_file)
             global contigs
             if contig_purity > purity.get(contig, 0):
-                purity[contig] = contig_purity
+                purity[contig] = (contig_purity, delta_homoeolog, delta_homolog)
         except(StopIteration):
             break
     return purity
@@ -18,17 +18,36 @@ def read_tsv(file_name):
 def tsv_reader(file_name):
     with open(file_name) as tsv_file:
         for line in tsv_file:
-            haplotype, contig, matched, total, matched_total_percent = line.split("\t")
-            yield haplotype, contig, float(matched_total_percent)
+            if line[0] == "h":
+                pass
+            else:
+                data = line.split("\t")
+                haplotype = data[0]
+                contig = data[1]
+                matched_total_percent = data[3]
+                delta_homoeolog = data[12]
+                delta_homolog = data[20]
+                yield haplotype, contig, float(matched_total_percent), float(delta_homoeolog), float(delta_homolog)
 
 
 def score(purity):
     impurity = 0.0
-    for contig, contig_purity in purity.iteritems():
-        print("{0}\t{1}".format(contig, contig_purity))
+    sum_homoeolog = 0.0
+    sum_homolog = 0.0
+    for contig, (contig_purity, delta_homoeolog, delta_homolog) in purity.iteritems():
+        print("{0}\t{1}\t{2}\t{3}".format(contig, contig_purity, delta_homoeolog, delta_homolog))
         if contig_purity < 1:
             impurity += 1
-    return impurity / len(purity)
+        sum_homoeolog += delta_homoeolog
+        sum_homolog += delta_homolog
+    delta = ""
+    if sum_homoeolog > sum_homolog:
+        delta = "homoeolog"
+    elif sum_homolog > sum_homoeolog:
+        delta = "homolog"
+    else:
+        delta = "tie"
+    return impurity / len(purity), delta, max(sum_homoeolog, sum_homolog)
 
 
 if __name__ == "__main__":
@@ -37,5 +56,6 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     purity = read_tsv(args.tsv)
-    print("IMPURITY: {0}".format(score(purity)))
+    impurity, explained_by, sum_percentage = score(purity)
+    print("IMPURITY: {0} DELTA: {1} ({2})".format(impurity, explained_by, sum_percentage))
 
