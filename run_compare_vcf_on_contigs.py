7fb6feeb8c02439c36d9a6dd07b62b0cef93ad76
Alison Paredes <ap2053@wildcats.unh.edu>
2018-04-30 15:39:34 -0400

    Runs other three sets of comparisons

--- run_compare_vcf_on_contigs.py
+++ run_compare_vcf_on_contigs.py
@@ -18,6 +18,28 @@ if __name__ == "__main__":
             command += " --homolog lambda_diploid_snp05_03.vcf "
             print(command)
             os.system(command)
+
+            command = "python /home/mcbs913_2018/shared/homoeologs_assembly/homoeolog_assembly/compare_vcf.py "
+            command += "lambda_diploid_snp05_02.vcf {0}".format(contig)
+            command += " --homoeolog lambda_diploid_snp05_01.vcf"
+            command += " --homolog lambda_diploid_snp05_04.vcf "
+            print(command)
+            os.system(command)
+
+            command = "python /home/mcbs913_2018/shared/homoeologs_assembly/homoeolog_assembly/compare_vcf.py "
+            command += "lambda_diploid_snp05_03.vcf {0}".format(contig)
+            command += " --homoeolog lambda_diploid_snp05_02.vcf"
+            command += " --homolog lambda_diploid_snp05_01.vcf "
+            print(command)
+            os.system(command)
+
+            command = "python /home/mcbs913_2018/shared/homoeologs_assembly/homoeolog_assembly/compare_vcf.py "
+            command += "lambda_diploid_snp05_04.vcf {0}".format(contig)
+            command += " --homoeolog lambda_diploid_snp05_01.vcf"
+            command += " --homolog lambda_diploid_snp05_02.vcf "
+            print(command)
+            os.system(command)
+
         except StopIteration:
             break
     exit(0)
