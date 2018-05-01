c0adad37ed204e2c1f093f9d9c7a76e98a7d6d5b
Alison Paredes <ap2053@wildcats.unh.edu>
2018-05-01 10:21:57 -0400

    Sums match counts instead of percentages

--- score_purity.py
+++ score_purity.py
@@ -25,15 +25,15 @@ def tsv_reader(file_name):
                 haplotype = data[0]
                 contig = data[1]
                 matched_total_percent = data[4]
-                delta_homoeolog = data[12]
-                delta_homolog = data[20]
+                delta_homoeolog = data[10]
+                delta_homolog = data[18]
                 yield haplotype, contig, float(matched_total_percent), float(delta_homoeolog), float(delta_homolog)
 
 
 def score(purity):
     impurity = 0.0
-    sum_homoeolog = 0.0
-    sum_homolog = 0.0
+    sum_homoeolog = 0
+    sum_homolog = 0
     for contig, (contig_purity, delta_homoeolog, delta_homolog) in purity.iteritems():
         print("{0}\t{1}\t{2}\t{3}".format(contig, contig_purity, delta_homoeolog, delta_homolog))
         if contig_purity < 1:
@@ -56,6 +56,6 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     purity = read_tsv(args.tsv)
-    impurity, explained_by, sum_percentage = score(purity)
-    print("IMPURITY: {0} DELTA: {1} ({2})".format(impurity, explained_by, sum_percentage))
+    impurity, explained_by, delta = score(purity)
+    print("IMPURITY: {0} DELTA: {1} ({2})".format(impurity, explained_by, delta))
 
