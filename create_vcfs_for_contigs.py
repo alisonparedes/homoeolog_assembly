50030142c4997ff03b1d23e9114899ee0d206f6b
Alison Paredes <ap2053@wildcats.unh.edu>
2018-05-06 16:42:11 -0400

    Blocks

--- create_vcfs_for_contigs.py
+++ create_vcfs_for_contigs.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     for line in sys.stdin:
         contig_file = line.strip()
 
-        command = "cat {0} {1} | muscle -o ref_{0}.fa ".format(contig_file, args.root_file)
+        command = "cat {0} {1} | muscle -out ref_{0}.fa ".format(contig_file, args.root_file)
         print(command, file=sys.stderr)
         result = os.system(command)
 
