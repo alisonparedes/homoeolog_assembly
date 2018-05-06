ff3c6d8e9e61acfdcd20cb026ce91c79ecdbbaec
Alison Paredes <ap2053@wildcats.unh.edu>
2018-05-06 16:32:47 -0400

    Blocks

--- create_vcfs_for_contigs.py
+++ create_vcfs_for_contigs.py
@@ -13,13 +13,16 @@ if __name__ == "__main__":
     for line in sys.stdin:
         contig_file = line.strip()
 
-        command = "cat {0} {1} | muscle ".format(contig_file, args.root_file)
-        command += " | csplit --prefix ref_{0}_ ref_{0}.fa ".format(contig_file)
+        command = "cat {0} {1} | muscle -o ref_{0}.fa ".format(contig_file, args.root_file)
+        print(command, file=sys.stderr)
+        result = os.system(command)
+
+        command = "csplit --prefix ref_{0}_ ref_{0}.fa ".format(contig_file)
         command += "'/>/' '{*}'"
         print(command, file=sys.stderr)
-        os.system(command)
+        result = os.system(command)
 
         command = "python3 /home/mcbs913_2018/shared/homoeologs_assembly/homoeolog_assembly/compare_fasta.py {0}".format(args.root_file)
         command += " ref_{0}_01 ref_{0}.vcf".format(contig_file)
         print(command, file=sys.stderr)
-        os.system(command)
+        result = os.system(command)
